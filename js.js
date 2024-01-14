5630e0d8047d2ea5c951a8182cf6defc15c4c2a9
dedi gusriyanto <dedigusriyanto@gmail.com>
2024-01-15 00:12:07 +0700

    Update

--- js.js
+++ js.js
@@ -6,5 +6,15 @@ function hitung(){
 }
 function riset() {
     document.getElementById('squareSide').value = "";
-    document.getElementById('luas').value = "";
+    document.getElementById('Luas').value = "";
+}
+function hitungkeliling(){
+    var inputkeliling = document.getElementById('inputkeliling').value;
+    var Keliling = 4 * inputkeliling;
+
+    document.getElementById("Keliling").innerHTML =  Keliling;
+}
+function risetkeliling() {
+    document.getElementById('inputkeliling').value = "";
+    document.getElementById('Keliling').value = "";
 }
