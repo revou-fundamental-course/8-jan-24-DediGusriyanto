f1598c48f7e2e50e7c3c81880105e50f0589a6e4
dedi gusriyanto <dedigusriyanto@gmail.com>
2024-01-16 00:44:37 +0700

    push

--- js/js.js
+++ js/js.js
@@ -1,8 +1,7 @@
 function hitung(){
-            var squareSide = document.getElementById('squareSide').value;
-            var Luas = squareSide * squareSide;
-
-			document.getElementById("Luas").innerHTML =  Luas;
+    var squareSide = document.getElementById('squareSide').value;
+    var Luas = squareSide * squareSide;
+    document.getElementById("Luas").innerHTML =  Luas;
 }
 function riset() {
     document.getElementById('squareSide').value = "";
