db99068d8575bb506136325cc22c3c3198b21496
dedi gusriyanto <dedigusriyanto@gmail.com>
2024-01-17 23:27:12 +0700

    Update

--- js/js.js
+++ js/js.js
@@ -1,19 +1,36 @@
 function hitung(){
-    var squareSide = document.getElementById('squareSide').value;
+    var squareSide = document.getElementById("squareSide").value;
     var Luas = squareSide * squareSide;
+
+    if (isNaN(squareSide)) {
+        alert("masukan harus dengan angka");
+        return;
+    }
+
     document.getElementById("Luas").innerHTML =  Luas;
 }
+
 function riset() {
     document.getElementById('squareSide').value = "";
-    document.getElementById('Luas').value = "";
+
+    document.getElementById('Luas').innerHTML= "";
 }
+
 function hitungkeliling(){
     var inputkeliling = document.getElementById('inputkeliling').value;
     var Keliling = 4 * inputkeliling;
 
+    if (isNaN(inputkeliling)){
+        alert("masukan dengan berupa angka.");
+
+        return;
+    }
+    
     document.getElementById("Keliling").innerHTML =  Keliling;
 }
+
 function risetkeliling() {
     document.getElementById('inputkeliling').value = "";
-    document.getElementById('Keliling').value = "";
+
+    document.getElementById('Keliling').innerHTML= "";
 }
